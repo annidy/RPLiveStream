9160d8f7d4859aa5840b4b7ac5da55d67346abea
annidy <annidy@gmail.com>
2018-02-08 15:38:34 +0800

    Initial Commit

---
+++ RPLiveStream/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RPLiveStream
+//
+//  Created by annidyfeng on 2018/2/8.
+//  Copyright © 2018年 annidy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
